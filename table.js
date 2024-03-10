725171d8dc1040e273f93b38213aa463f2f1bf6f
unknown <ha5755420@gmail.comgit config --global user.name Syed-Hamza-Ali-8>
2024-03-09 23:40:58 -0800

    Table

---
+++ table.js
@@ -0,0 +1,4 @@
+var enterNumberToGenerateTable = prompt("Enter a number to generate it's table")
+for (i = 1; i < 11; i++) {
+console.log(enterNumberToGenerateTable, "x" , i , "=" ,enterNumberToGenerateTable*i)
+}
